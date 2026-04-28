e851162dc8ac21a1e5004bbc887dae38d8a7266e
HanzSun <HanzalaSundal@gmail.com>
2026-04-27 22:43:11 -0500

    decuple

--- bonus/bonus11.py
+++ bonus/bonus11.py
@@ -1,16 +1,18 @@
 feet_inches = input("Enter feet and inches: ")
 
-
-def convert(feet_inches):
-    parts = feet_inches.split(" ")
+def parse(feetinches):
+    parts = feetinches.split(" ")
     feet = float(parts[0])
     inches = float(parts[1])
+    return feet, inches
 
+def convert(feet, inches):
     meters = feet * 0.3048 + inches * 0.0254
     return meters
 
 
-results = convert(feet_inches)
+f, i = parse(feet_inches)
+results = convert(f, i)
 
 if results < 1:
     print("Kids is too small")
