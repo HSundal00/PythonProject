bdb7b6d78f7e1723b90e6520dd548e64936dbebb
HanzSun <HanzalaSundal@gmail.com>
2026-04-28 23:15:35 -0500

    bonus modules

--- bonus/bonus11.py
+++ bonus/bonus11.py
@@ -1,14 +1,7 @@
+from bonus.parsers import parse
+from bonus.converters import parse
+
 feet_inches = input("Enter feet and inches: ")
-
-def parse(feetinches):
-    parts = feetinches.split(" ")
-    feet = float(parts[0])
-    inches = float(parts[1])
-    return feet, inches
-
-def convert(feet, inches):
-    meters = feet * 0.3048 + inches * 0.0254
-    return meters
 
 
 f, i = parse(feet_inches)
